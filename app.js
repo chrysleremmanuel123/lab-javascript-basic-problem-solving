3f58bb877f903c303f8b293c5dda063ea7a1c2c8
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-07-22 00:04:24 +0530

    lab-1

--- app.js
+++ app.js
@@ -2,35 +2,111 @@
 // Progression 1: Names and Input
 
 // 1.1 Create a variable `ProGrad-1` with the driver's name.
+var driverName = "mayank";
 // 1.2 Print `"The driver's name is XXXX"`.
+console.log(driverName);
 // 1.3 Create a variable `ProGrad-2` with the navigator's name.
+var navigatorName = "Chrysler"
 // 1.4 Print `"The navigator's name is YYYY"`.
+console.log(navigatorName);
 
 // Progression 2: Control Statements - 1
 // 2.1. Depending on which name is longer, print:
 // - The driver has the longest name, it has XX characters. or
 // - It seems that the navigator has the longest name, it has XX characters. or
 // - Wow, you both have equally long names, XX characters!.
+if (driverName.length > navigatorName.length) {
+    console.log("The driver has the longest name,it has " + driverName.length + " characters");
+}
+else if (navigatorName.length > driverName.length) {
+    console.log("It seems that the navigator has the longest name,it has " + navigatorName.length + " characters");
+}
+else if (navigatorName.length == driverName.length) {
+    console.log("Wow you both have equally long names, " + navigatorName.length + " characters");
+}
 
 // 2.2. Check if the string contains vowels or not.
 // - If it contains vowels, print the name, and also print the vowel letters along with the vowel index. or
 // - print no vowels
 // - for example. In String ProGrad - o and a are vowels. Print ProGrad o a 2 5. 
+function getVowels(str) {
+    var vowelsCount = 0;
 
+    //turn the input into a string
+    var string = str.toString();
+
+    //loop through the string
+    for (var i = 0; i <= string.length - 1; i++) {
+
+        //if a vowel, add to vowel count
+        if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
+            vowelsCount += 1;
+            console.log(string.charAt(i));
+        }
+    }
+    return vowelsCount;
+}
+console.log(getVowels(navigatorName));
+console.log(getVowels(driverName));
 // 2.3. Check if the string contains uppercase and lowercase characters Xx
-// - Print the number of upper case characters
-// - Print the number of lower case characters
+function upper_case(str) {
+    regexp = /[A-Z]/;
+    if (regexp.test(str)) {
+        console.log("String contains uppercase characters");
+    }
+    else {
+        console.log("String does not contain uppercase characters");
+    }
+}
+upper_case(navigatorName);
+upper_case(driverName);
 
+// - Print the number of upper case characters
+function count(str) {
+
+    var numUpper = (str.match(/[A-Z]/g) || []).length;
+    console.log(numUpper);
+}
+count(navigatorName);
+count(driverName);
+// - Print the number of lower case characters
+function count(str) {
+
+    var numUpper = (str.match(/[a-z]/g) || []).length;
+    console.log(numUpper);
+}
+count(navigatorName);
+count(driverName);
 
 // Progression 3: Control Statements - 2
 // 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
-
+var res = driverName.split("");
+console.log(res);
+console.log(res.join(" "));
 // 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
-
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString(navigatorName));
 // 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
+var result = driverName.concat(navigatorName);
+console.log(result);
 // - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
-
+var result1 = navigatorName.concat(driverName);
+console.log(result1);
 // 3.3 Depending on the lexicographic order of the strings, print:
+function strcmp(navigatorName, driverName) {
+    if (navigatorName === driverName) {
+        console.log("What?! You both have the same name?");
+    }
+    else if (navigatorName > driverName) {
+        console.log("Yo,the navigator goes first definitely");
+    }
+    else if (driverName > navigatorName) {
+        console.log("The driver goes first");
+    }
+}
+console.log(strcmp(driverName, navigatorName));
 // - The driver's name goes first.
 // - Yo, the navigator goes first definitely.
 // - What?! You both have the same name?
